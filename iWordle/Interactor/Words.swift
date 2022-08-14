e3ca786f5d2f732ca5cbd21d77b79537f55c5571
valseLee <sollleky72@gmail.com>
2022-08-14 21:12:17 +0900

    add: GET한 단어 리스트 저장용 구조체

---
+++ iWordle/Interactor/Words.swift
@@ -0,0 +1,16 @@
+//
+//  Word.swift
+//  iWordle
+//
+//  Created by 이승준 on 2022/08/14.
+//
+
+import Foundation
+
+struct Words {
+	var wordList: [String]?
+	
+	init(words: [String]) {
+		self.wordList = words
+	}
+}
