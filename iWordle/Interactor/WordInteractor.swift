26706f9b26042880c640f31bc63053e0f3257db9
valseLee <sollleky72@gmail.com>
2022-08-23 20:41:14 +0900

    update: 파일명 변경 및 수정

--- iWordle/Interactor/WordInteractor.swift
+++ iWordle/Interactor/WordInteractor.swift
@@ -1,18 +1,27 @@
 //
-//  WordCheckInteractor.swift
+//  Word.swift
 //  iWordle
 //
-//  Created by 이승준 on 2022/08/22.
+//  Created by 이승준 on 2022/08/14.
 //
 
 import Foundation
 
-// MARK: 싱글톤일 필요가 있는가 vs 싱글톤이 아닐 필요가 있는가 vs 다형성이 요구되지 않는가 vs 앱 내에서 일관적으로 통일되어 있어야 하는가
-final class WordInteractor {
-	static var apiWord: String? = "DEBUG: Word hasnt Init Yet"
-	static var userCharcter: String? = "DEBUG: User Input is wrong"
+final class Words {
+	typealias CheckSame = ([String], [String]) -> ()
+	static let shared = Words()
 	
-	static func wordChecker(indexPath row: Int, userInput text: String) {
+	lazy var apiWord: String? = "DEBUG: Word hasnt Init Yet"
+	lazy var userCharcter: String? = "DEBUG: User Input is wrong"
+	private var containsString: Bool = false
+	private var alignSamePosition: Bool = false
+	var userInput = ""
+	
+	func setWord(wordsList: [String]) {
+		apiWord = wordsList.randomElement()!.uppercased()
+	}
+	
+	public func wordCheck(indexPath row: Int, userInput text: String) {
 		if text != "" {
 			switch row {
 				case 0...4:
@@ -31,8 +40,18 @@ final class WordInteractor {
 			}
 		}
 	}
-	
-	static func setWord(wordsList: [String]) {
-		apiWord = wordsList.randomElement()!.uppercased()
+
+	func checkPosition() {
+		guard let apiWord = apiWord else { return }
+		var userInputCharacterArray = userInput.map { String($0.uppercased()) }
+		var apiWordCharacterArray = apiWord.map { String($0) }
+
+		for i in 0...4 {
+			if userInputCharacterArray[i] == apiWordCharacterArray[i] {
+				print(#function, "\(i)번째 일치")
+			}
+		}
 	}
+	
+	private init() {}
 }
