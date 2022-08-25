02089322fcce1ca829b79d8f14d8239edd9bc97d
valseLee <sollleky72@gmail.com>
2022-08-26 01:05:26 +0900

    update: 코드 가독성 향상 작업

--- iWordle/View/GameViewCell.swift
+++ iWordle/View/GameViewCell.swift
@@ -9,21 +9,6 @@ import UIKit
 
 final class GameViewCell: UICollectionViewCell {
 	
-//	lazy var usersInput: String = "" {
-//		didSet {
-//			chracterTextField.text = ""
-//			print(usersInput)
-//			mainText.text = usersInput
-//		}
-//	}
-	
-	lazy var mainText: UILabel = {
-		let la = UILabel()
-		la.font = UIFont.boldSystemFont(ofSize: 16)
-		la.textAlignment = .center
-		return la
-	}()
-	
 	public var chracterTextField: UITextField = {
 		let tf = UITextField()
 		tf.layer.borderColor = UIColor.black.cgColor
@@ -53,22 +38,14 @@ final class GameViewCell: UICollectionViewCell {
 		addSubview(chracterTextField)
 		chracterTextField.delegate = self
 		chracterTextField.setAnchorTRBL(top: topAnchor, right: rightAnchor, bottom: bottomAnchor, left: leftAnchor)
-		
-		addSubview(mainText)
-		mainText.setAnchor(anchorTo: [.top(padding: 5, isToSafeArea: false),
-									   .bottom(padding: 5, isToSafeArea: false),
-									   .leading(padding: 5, isToSafeArea: false),
-									   .trailing(padding: 5, isToSafeArea: false)],
-							inView: self)
 	}
 }
 
 extension GameViewCell: UITextFieldDelegate {
 	func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
 		guard let textFieldText = textField.text,
-			  let rangeOfTextToReplace = Range(range, in: textFieldText) else {
-			return false
-		}
+			  let rangeOfTextToReplace = Range(range, in: textFieldText) else { return false }
+		
 		let substringToReplace = textFieldText[rangeOfTextToReplace]
 		let count = textFieldText.count - substringToReplace.count + string.count
 		return count <= 1
@@ -76,12 +53,8 @@ extension GameViewCell: UITextFieldDelegate {
 
 	func textFieldDidEndEditing(_ textField: UITextField) {
 		guard let str = textField.text else { return }
-		if str != "" {
-			chracterTextField.isUserInteractionEnabled = false
-		}
+		if str != "" { chracterTextField.isUserInteractionEnabled = false }
 		
-		NotificationCenter.default.post(name: .textChanged,
-										object: (self, str),
-										userInfo: nil)
-	}
+		NotificationCenter.default.post(name: .textChanged,	object: (self, str), userInfo: nil)
+	}	
 }
